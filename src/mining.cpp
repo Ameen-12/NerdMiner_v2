f37235fda08eedb8bc5c6bc496c2104c39b0e95e
Mohammed khamruddin <63597933+Ameen-12@users.noreply.github.com>
2026-04-23 17:00:15 +0530

    Update mining.cpp

--- src/mining.cpp
+++ src/mining.cpp
@@ -1116,7 +1116,7 @@ void minerWorkerHw(void * task_id)
           }
         }
         if (
-             (uint8_t)(n & 0xFF) == 0 &&
+             (uint16_t)(n & 0x3FF) == 0 &&
              s_working_current_job_id != job_in_work)
         {
           result->nonce_count = n+1;
